a5d32b8eb15f3d8dd34a6b4ceeaa0eb1b74fc352
ChethanN17 <chethan.n@niveussolutions.com>
2024-03-01 10:10:20 +0530

    # Initial server setup 1. gitignore added 2. package.json added 3. server.js added

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+// Step 1: Create the express application
+const app = express();
+
+// Step 2: Define the routes for the application 
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+/// Step 3: Start the web server and listen for
+/// incoming connections on a specified host and port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
