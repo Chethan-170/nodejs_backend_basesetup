8a499047a6455e2996764c156ba953a8379a33ba
Chethan-170 <chethanpoojary170@gmail.com>
2024-03-01 15:29:40 +0530

    cookie-parser added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(cookieParser());
 
 // routes import
 import userRouter from './routes/user.routes.js'
